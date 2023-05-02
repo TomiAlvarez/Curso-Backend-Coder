158949c99c965b42c36b0eb08c86556d249b267c
Tomas Alvarez <tomasalv569@gmail.com>
2023-05-02 11:49:44 -0300

    +websockets

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import productsRouter from './routers/products.router.js'
 import cartsRouter from './routers/carts.router.js'
+import { Server } from 'socket.io'
 
 const app = express()
 app.use(express.json())
@@ -10,4 +11,10 @@ app.use(express.static('public'))
 app.use('/api', productsRouter)
 app.use('/api', cartsRouter)
 
-app.listen(8080, () => console.log('Server Up'))
+const httpServer = app.listen(8080, () => console.log('Server Up'))
+
+const socketServer = new Server(httpServer)
+
+socketServer.on('connection', () => {
+    console.log('Socket client connected...')
+})
